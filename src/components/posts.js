0c639a941423a4893ab6e61dfe115972a1592120
Pirun Seng <sengpirun.rain@gmail.com>
2018-06-15 16:39:50 +0700

    Returning Array available in React version 16

--- src/components/posts.js
+++ src/components/posts.js
@@ -2,13 +2,28 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Posts = () => {
-    return (
-      <div>
-        <Link to='/posts/1'>Post 1</Link><br/>
-        <Link to='/posts/2'>Post 2</Link><br/>
-        <Link to='/posts/3'>Post 3</Link><br/>
-      </div>
-    )
+    // const arr = [
+    //   {id: '1', name: 'Post 1'},
+    //   {id: '2', name: 'Post 2'},
+    //   {id: '3', name: 'Post 3'}
+    // ]
+    // const list = arr.map( item => {
+    //   return(
+    //     <span key={item.id}>
+    //     <Link to={`/posts/${item.id}`}>{item.name}</Link><br/>
+    //     </span>
+    //   )
+    // })
+    // return (
+    //   <div>
+    //     { list }
+    //   </div>
+    // )
+    return [
+      <div key='1'>Hello!</div>,
+      <div key='2'>React</div>,
+      <div key='3'>is awesome!</div>
+    ]
 }
 
 export default Posts;
